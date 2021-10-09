a8003ee78ebe23b446a196c5f9935b51559d9c80
Nikhilesh Jain <nikhileshjain@Nikhileshs-MacBook-Pro.local>
2021-10-09 18:39:42 +0530

    view engine setup

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port =8000;
 //used express router
 app.use('/',require('./routes/index')); 
 
+// set up of view engine
+app.set('view engine','ejs');
+app.set('views', './views'); 
+
 app.listen(port, function(err){
     if (err)
     {
