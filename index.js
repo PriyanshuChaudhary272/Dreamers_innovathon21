69cf54625857f6e55e92adc61678df77c869d158
nikhileshjain <nikhilesh.jain07@gmail.com>
2021-10-10 13:52:18 +0530

    installed some more packages

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotnev = require('dotenv');
+const helmet = require('helmet');
+const morgon = require('morgan');
+
 const app = express();
 const port =8000;
+
+
+dotnev.config();
+
+mongoose.connect(process.env.MONGO_URL,{useNewUrlParser: true, useUnifiedTopology: true},()=>{
+    console.log("Connected to MongoDB")
+});
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgon("common"));
 
 //used express router
 app.use('/',require('./routes/index')); 
